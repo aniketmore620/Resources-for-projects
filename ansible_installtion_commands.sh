9d9301313076c71a7530bfa46f81dd06184557c5
aniketmore620 <145662770+aniketmore620@users.noreply.github.com>
2024-03-04 19:58:07 +0530

    Create ansible_installtion_commands.sh

---
+++ ansible_installtion_commands.sh
@@ -0,0 +1,5 @@
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+sudo apt-get install python-software-properties # You are probably missing the python-software-properties package Install it using the following command.
+ansible --version
